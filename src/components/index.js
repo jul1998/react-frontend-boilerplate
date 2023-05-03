dbf281757b90326a11d0619564d8072123e4f1e8
jul1998 <jguevara321@gmail.com>
2023-05-02 22:48:59 -0600

    first commit for frontend

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export {default as Home} from './Home'
+export {default as Navbar} from './Navbar'
+export {default as SignupPage} from './SignupPage'
+export {default as LogoutComp} from './LogoutComp'
